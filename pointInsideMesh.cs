855b9318f7d5d4fca2ddea991e671995c5e11ca8
Tomaso <66296094+IsaakThaddaeus@users.noreply.github.com>
2024-09-23 21:00:04 +0200

    Add files via upload

--- pointInsideMesh.cs
+++ pointInsideMesh.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public static class pointInsideMesh
 {
+    // Checks if a point is inside a mesh
     public static bool isPointInsideMesh(GameObject meshObject, Vector3 point)
     {
         Mesh mesh = meshObject.GetComponent<MeshFilter>().mesh;
@@ -35,7 +39,11 @@ public static class pointInsideMesh
         return numberOfIntersections % 2 != 0;
     }
 
-    //Seems to be twice as fast as the above, bounding box check should be included
+
+    // Checks if a point is inside a mesh more efficiently.
+    // This implementation is approximately twice as fast as the previous one.
+    // To further optimize, consider adding a bounding box check to quickly eliminate triangles
+    // that are outside the relevant region, reducing unnecessary intersection tests.
     public static bool isPointInsideMeshFast(GameObject meshObject, Vector3 point)
     {
         Mesh mesh = meshObject.GetComponent<MeshFilter>().mesh;
@@ -86,6 +94,48 @@ public static class pointInsideMesh
 
         return numberOfIntersections % 2 != 0;
     }
+
+
+    // Computes the closest point on the surface of the mesh from a point located inside the mesh 
+    public static Vector3 closestPointOnMesh(GameObject meshObject, Vector3 point)
+    {
+        Mesh mesh = meshObject.GetComponent<MeshFilter>().mesh;
+
+        Vector3[] vertices = mesh.vertices;
+        int[] triangles = mesh.triangles;
+        Matrix4x4 localToWorld = meshObject.transform.localToWorldMatrix;
+
+        Vector3 closestPoint = Vector3.zero;
+
+        for (int i = 0; i < triangles.Length; i += 3)
+        {
+            Vector3 v0 = vertices[triangles[i]];
+            Vector3 v1 = vertices[triangles[i + 1]];
+            Vector3 v2 = vertices[triangles[i + 2]];
+
+            v0 = localToWorld.MultiplyPoint3x4(v0);
+            v1 = localToWorld.MultiplyPoint3x4(v1);
+            v2 = localToWorld.MultiplyPoint3x4(v2);
+
+
+            Vector3 edge1 = v1 - v0;
+            Vector3 edge2 = v2 - v0;
+
+            Vector3 rayDirection = Vector3.Cross(edge1, edge2);
+
+            if (IntersectRayTriangle(point, rayDirection, v0, v1, v2, out float t))
+            {
+                if(closestPoint == Vector3.zero || Vector3.Distance(point, point + rayDirection * t) < Vector3.Distance(point, closestPoint)){
+                    closestPoint = point + rayDirection * t;
+                }
+            }
+        }
+
+        return closestPoint;
+    }
+
+
+    // Executes the Möller-Trumbore algorithm to determine if a ray intersects with a given triangle.
     public static bool IntersectRayTriangle(Vector3 rayOrigin, Vector3 rayDirection, Vector3 v0, Vector3 v1, Vector3 v2, out float t)
     {
         t = 0;
