ec91e4c32748dcf8338c77b4f5838ebca0dcb3ec
Tomaso <66296094+IsaakThaddaeus@users.noreply.github.com>
2024-09-23 21:01:36 +0200

    Add files via upload

--- pointInsideMesh.cs
+++ pointInsideMesh.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Drawing;
-using UnityEditor.Rendering;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public static class pointInsideMesh
 {
